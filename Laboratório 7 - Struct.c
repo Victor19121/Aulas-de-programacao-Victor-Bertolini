9761284ade86ce9ddad8e50f2cd1a6fba0fee4fe
Victor Bertolini de Sousa <103936911+Victor19121@users.noreply.github.com>
2022-12-15 23:43:30 -0300

    Update Laboratório 7 - Struct.c

--- Laboratório 7 - Struct.c
+++ Laboratório 7 - Struct.c
@@ -145,6 +145,7 @@ main()
 // d) Encontre o aluno com menor média geral;
 // e) Para cada aluno diga se ele foi aprovado ou reprovado, considerando o valor 6 para aprovação.
 #include <stdio.h>
+#include <string.h>
 
 typedef struct
 {
@@ -197,7 +198,16 @@ main()
     for (int count = 0; count < 5; count++)
     {
         if (higher_1 < student[count].test1)
+        {
             higher_1 = student[count].test1;
+        }
+            
+    }
+
+
+    for (int count = 0; count < 5; count++)
+    {
+        
 
         average = (student[count].test1 + student[count].test2 + student[count].test3) / 3;
 
@@ -220,3 +230,324 @@ main()
 
 }
 ===================================================
+// 5. Considerando a estrutura
+// struct vetor {
+// float a;
+// float b;
+// float c;
+// };
+// para representar um vetor no R3, implemente um programa que calcule a soma de dois
+// vetores.
+#include <stdio.h>
+#include <math.h>
+
+typedef struct
+{
+float a;
+float b;
+float c;
+
+} Vetorization;
+
+void
+main()
+{
+    char 
+        letter = 88;
+    
+    float 
+        U = 0,
+        V = 0;
+
+    Vetorization vetor[2];
+   
+
+    for (int count = 0; count < 2; count++)
+    {
+        printf("Enter the position X of the vector %d: ", count);
+        scanf("%f", &vetor[count].a);
+
+        printf("Enter the position Y of the vector %d: ", count);
+        scanf("%f", &vetor[count].b);
+
+        printf("Enter the position Z of the vector %d: ", count);
+        scanf("%f", &vetor[count].c);
+    }
+
+    U = sqrt((vetor[0].a * vetor[0].a) + (vetor[0].b * vetor[0].b) + (vetor[0].c * vetor[0].c));
+    V = sqrt((vetor[1].a * vetor[1].a) + (vetor[1].b * vetor[1].b) + (vetor[1].c * vetor[1].c));
+
+    printf("The resultant of the sum is: %f\n", U + V);  
+}
+===========================================================================================
+// 6. Faça um programa que armazene em um registro de dados (estrutura composta) os dados
+// de um funcionário de uma empresa, compostos de: Nome, Idade, Sexo (M/F), CPF, Data de
+// Nascimento, Código do Setor onde trabalha (0-99), Cargo que ocupa (string de até 30
+// caracteres) e Salário. Os dados devem ser digitados pelo usuário, armazenados na estrutura
+// e exibidos na tela.
+#include <stdio.h>
+#include <string.h>
+
+typedef struct
+{
+    char 
+        nome[30],
+        sexo,
+        cargo[30];
+    int 
+        idade,
+        dia_nas,
+        mes_nas,
+        ano_nas,
+        setor,
+        salario;
+    
+    float
+        CPF;
+
+} ficha_funcionario;
+
+
+void
+main()
+{
+    ficha_funcionario funcionario;
+
+    printf("Digite o nome: ");
+    scanf("%30[^\n]", &funcionario.nome);
+
+    fflush(stdin);
+    printf("Digite M ou F para seu sexo: ");
+    scanf("%c", &funcionario.sexo);
+
+    fflush(stdin);
+    printf("Digite o seu cargo: ");
+    scanf("%30[^\n]", &funcionario.cargo);
+
+    printf("Digite sua idade: ");
+    scanf("%d", &funcionario.idade);
+
+    printf("Digite o seu CPF: ");
+    scanf("%f", &funcionario.CPF);
+
+    printf("Digite a sua data de nascimento no formato 19 12 2003: ");
+    scanf("%d %d %d", &funcionario.dia_nas, &funcionario.mes_nas, &funcionario.ano_nas);
+
+    printf("Digite o numero do seu setor: ");
+    scanf("%d", &funcionario.setor);
+
+    printf("Digite o seu salário: ");
+    scanf("%d", &funcionario.salario);
+
+
+    printf("Nome: %s\n", funcionario.nome);
+    printf("Sexo: %c\n", funcionario.sexo);
+    printf("Cargo: %s\n", funcionario.cargo);
+    printf("Idade: %d\n", funcionario.idade);
+    printf("CPF: %.0f\n", funcionario.CPF);
+    printf("data de nascimento: %d/%d/%d\n", funcionario.dia_nas, funcionario.mes_nas, funcionario.ano_nas);
+    printf("Setor: %d\n", funcionario.setor);
+    printf("Salario: %d\n", funcionario.salario);
+}
+==========================================================================================
+// 7. Utilizando uma estrutura, faça um programa que permita a entrada de nome, endereço e
+// telefone de 5 pessoas e os imprima em ordem alfabética.
+#include <stdio.h>
+#include <string.h>
+
+typedef struct 
+{   
+    char 
+        nome[30],
+        endereco[100];
+    float
+        telefone;
+}Ficha_pessoa;
+
+
+void
+main()
+{
+    char    
+        alfabeto_M[26] = {'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'},
+        alfabeto_m[26] = {'a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'};
+
+    printf("%c", alfabeto_M[0]);
+    printf("%c", alfabeto_m[0]);
+
+    Ficha_pessoa pessoa[5];
+
+    for (int count = 0; count < 5; count++)
+    {
+        fflush(stdin);
+        printf("Digite o nome da pessoa %d: ", count + 1);
+        scanf("%30[^\n]", &pessoa[count].nome);
+
+        fflush(stdin);
+        printf("Digite o endereço da pessoa: ");
+        scanf("%100[^\n]", &pessoa[count].endereco);
+
+        printf("Digite o telefone: ");
+        scanf("%f", &pessoa[count].telefone);
+    }
+
+    for (int count = 0; count < 26; count++)
+    {
+        if (pessoa[0].nome[0] == alfabeto_M[count] || pessoa[0].nome[0] == alfabeto_m[count])
+            printf("Nome: %s\n Endereco: %s\n Telefone: %.0f\n\n", pessoa[0].nome, pessoa[0].endereco, pessoa[0].telefone);
+
+        if (pessoa[1].nome[0] == alfabeto_M[count] || pessoa[1].nome[0] == alfabeto_m[count])
+            printf("Nome: %s\n Endereco: %s\n Telefone: %.0f\n\n", pessoa[1].nome, pessoa[1].endereco, pessoa[1].telefone);
+
+        if (pessoa[2].nome[0] == alfabeto_M[count] || pessoa[2].nome[0] == alfabeto_m[count])
+            printf("Nome: %s\n Endereco: %s\n Telefone: %.0f\n\n", pessoa[2].nome, pessoa[2].endereco, pessoa[2].telefone);
+
+        if (pessoa[3].nome[0] == alfabeto_M[count] || pessoa[3].nome[0] == alfabeto_m[count])
+            printf("Nome: %s\n Endereco: %s\n Telefone: %.0f\n\n", pessoa[3].nome, pessoa[3].endereco, pessoa[3].telefone);
+
+        if (pessoa[4].nome[0] == alfabeto_M[count] || pessoa[4].nome[0] == alfabeto_m[count])
+            printf("Nome: %s\n Endereco: %s\n Telefone: %.0f\n\n", pessoa[4].nome, pessoa[4].endereco, pessoa[4].telefone);
+    }
+    
+}
+=======================================================================================
+// 8. Escolha um jogo de cartas, baseado em um “baralho tradicional” (cada carta tem seu naipe
+// e seu valor). Implemente a parte de distribuição (sorteio) de cartas para 2 jogadores,
+// considerando que cada jogador irá receber 3 cartas. Exiba na tela as cartas que cada um dos
+// jogadores recebeu.
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+#include <string.h>
+
+typedef struct
+{
+    char tipo[4][10]
+
+} Naipe;
+
+
+typedef struct
+{
+    Naipe naipe_carta;
+    int valor;
+
+} Baralho;
+
+void
+main()
+{
+    int 
+        random = 0,
+        random_naipe = 0;
+
+    Baralho 
+        baralho;
+
+    srand(time(NULL));
+
+    strcpy(baralho.naipe_carta.tipo[0], "Ouro");
+    strcpy(baralho.naipe_carta.tipo[1], "Paus");
+    strcpy(baralho.naipe_carta.tipo[2], "Copas");
+    strcpy(baralho.naipe_carta.tipo[3], "Espadas");
+
+    //Jogador 1
+    printf("O jogador 1 possui as cartas:\n");
+    for (int count = 0; count < 3; count++)
+    {
+        random = (rand() % 9) + 1;
+        random_naipe = (rand() % 3) + 1;
+        printf("%d de %s\n", random, baralho.naipe_carta.tipo[random_naipe]);
+    }
+
+    //Jogador 2
+    printf("O jogador 2 possui as cartas:\n");
+    for (int count = 0; count < 3; count++)
+    {
+        random = (rand() % 9) + 1;
+        random_naipe = (rand() % 3) + 1;
+        printf("%d de %s\n", random, baralho.naipe_carta.tipo[random_naipe]);
+    }
+}
+=================================================================================
+// 9. Faça um programa que leia um vetor com os dados de 5 carros: marca (máximo 15 letras),
+// ano e preço. Leia um valor p e mostre as informações de todos os carros com preço menor
+// que p. Repita este processo até que seja lido um valor p = 0.
+#include <stdio.h>
+#include <string.h>
+#include <locale.h>
+
+//Struct de informações do carro
+typedef struct 
+{
+    char 
+        marca[15];
+
+    int
+        ano,
+        preco;
+} Ficha_Carro;
+
+//Função para limpar o teclado
+void
+clean()
+{
+    fflush(stdin);
+}
+
+
+int 
+main() 
+{
+    Ficha_Carro     //Chamando a struct
+        carro[5];
+    char
+        aprovados[5]; //carros que passaram no teste de preço
+    int 
+        valor_max = 0, 
+        contador = 0,
+        tamanho = 0,
+        index = 0;
+
+    //MIMIMI meu 
+    setlocale(LC_ALL, "Portuguese");
+
+    //Pegando o preço que está disposto a pagar
+    printf("Digite o preço que você está disposto a pagar por um carro: ");
+    scanf("%d", &valor_max);
+
+    //Um loop para suprir as structs com informações sobre os veículos
+    for (int count = 0; count < 5; count++)
+    {
+        printf("Carro %d\n", count + 1);
+        clean();
+        printf("Digite a marca do carro: ");
+        scanf("%15[^\n]", &carro[count].marca);
+
+        printf("Digite o ano que foi fabricado: ");
+        scanf("%d", &carro[count].ano);
+
+        printf("Digite o preco do carro: ");
+        scanf("%d", &carro[count].preco);
+
+        if(carro[count].preco <= valor_max)
+        {
+            aprovados[contador] = count;
+            contador++;
+        }
+        printf("========================\n");
+    } 
+
+    tamanho = strlen(aprovados);
+    if (tamanho > 0)
+    {
+        for (int count = 0; count < tamanho; count++)
+        {
+            index = aprovados[count];
+            printf("Marca: %s\nAno: %d\nPreco: %d\n", carro[index].marca, carro[index].ano, carro[index].preco);
+        }
+    } 
+}
+===========================================================================
+
